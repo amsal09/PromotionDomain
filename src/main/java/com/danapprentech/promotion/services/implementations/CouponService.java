2abee63037dbe3808db4a0bc671d775787e68efe
Amsal Jayadi Sihombing <sihombingamsal@gmail.com>
2019-10-18 13:49:26 +0700

    update timezone

--- src/main/java/com/danapprentech/promotion/services/implementations/CouponService.java
+++ src/main/java/com/danapprentech/promotion/services/implementations/CouponService.java
@@ -17,9 +17,7 @@ import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import javax.transaction.Transactional;
 import java.util.*;
 
